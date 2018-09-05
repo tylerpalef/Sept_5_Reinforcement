7c169aadee9ff52620fb8094d56ff0093b118951
“Tyler <tylerpalef@gmail.com>
2018-09-05 18:42:18 -0400

    finished exer 2 almost

---
+++ exer2.js
@@ -0,0 +1,36 @@
+// apples = 14
+// puts apples
+//
+// puts "I have #{apples} apples."
+
+var apples = 14
+console.log('I have' + ' ' + apples + ' '+ 'apples')
+
+// materials = ['wood', 'metal', 'stone']
+// words = {
+//   'elephant' => "The world's largest land mammal.",
+//   'school' => 'A place of learning.',
+//   'ice cream' => 'A delicious milk-based dessert.',
+// }
+
+var materials = ['wood', 'metal', 'stone']
+
+console.log((materials[0]) + ' ' + words)
+
+// num = 16
+// if num > 10
+//   puts "#{num} is greater than 10."
+// elsif num == 10
+//   puts "#{num} is exactly 10."
+// else
+//   puts "#{num} must be less than 10."
+// end
+
+var num = 16
+  if num > 10
+  console.log('(num) is greater than 10.')
+  elsif num === 10
+  console.log('(num) is exactly 10.')
+  else
+  console.log('(num) must be lass than 10.')
+end
